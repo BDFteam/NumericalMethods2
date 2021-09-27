64998cd02295406a4f60b7ae745b3466cfe36457
Cemre Süler <Cemre8@live.nl>
2021-09-27 21:39:18 +0200

    Bedankt voor het broodje kipkerrie Boef
    Tis je gegund neef

--- ex2a.py
+++ ex2a.py
@@ -1,3 +1,36 @@
 #DO NOT SEND THIS FILE BY E-MAIL!
 # - Cemre
 
+import numpy as np
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+n = int(input("enter n: "))
+m = int(input("enter m: "))
+
+array = [[0 for i in range(m)] for j in range(n)]
+
+def rmbrg(a,b, n,m):
+
+    for i in range(n):
+        if(i == 0):
+            array[i][0] = 0.5 * (b-a) * (f(a)+f(b))
+
+        if(i != 0):
+            sum = 0
+            for k in range((2**(i-1))):
+                sum += f(a + ((2*k)-1) * ((b-a)/(2**i)))
+            array[i][0] = (0.5 * array[i-1][0]) + ((((b-a)/(2**i))) * sum)
+        
+        for j in range(m):
+            if(i != 0 and j != 0):
+                array[i][j] = array[i][j-1] + (1/((4**j)-1)) * (array[i][j-1] - array[i-1][j-1])
+                
+def f(x):
+    return(4/(1+(x**2)))
+
+rmbrg(a,b,n,m)
+
+for n in array:
+    print(n)
+    
