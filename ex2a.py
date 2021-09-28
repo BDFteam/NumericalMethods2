b6b00a998b41678458506f96be8ee6e19da5548a
Cemre Süler <c.suler@student.vu.nl>
2021-09-28 07:22:50 +0000

    Ik ben in de buurt ik moet hosselen

--- ex2a.py
+++ ex2a.py
@@ -8,7 +8,8 @@ b = int(input("enter b: "))
 n = int(input("enter n: "))
 m = int(input("enter m: "))
 
-array = np.zeros((n, m))
+array = np.zeros((n, m), dtype=np.float128)
+print(array)
 
 def rmbrg(a,b, n,m):
     array[0][0] = 0.5 * (b-a) * (f(a)+f(b))
@@ -20,7 +21,7 @@ def rmbrg(a,b, n,m):
                 sum += f(a + ((2*(k+1))-1) * ((b-a)/(2**i)))
             array[i][0] = (0.5 * array[i-1][0]) + ((((b-a)/(2**i))) * sum)
         
-        for j in range(m):
+        for j in range(min(m, i + 1)):
             if(i != 0 and j != 0):
                 array[i][j] = array[i][j-1] + (1/((4**j)-1)) * (array[i][j-1] - array[i-1][j-1])
                 
