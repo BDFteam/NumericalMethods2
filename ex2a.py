d3a9b4df6f0c937d64c8227dd26a668068159d0e
Micha den Heijer <micha@michadenheijer.com>
2021-09-29 14:23:01 +0200

    Dit is een geen boefquote

--- ex2a.py
+++ ex2a.py
@@ -1,25 +1,35 @@
 # DO NOT SEND THIS FILE BY E-MAIL!
 # - Cemre
+from typing import Callable
 
 import numpy as np
 
 
-def rmbrg():
-    a = int(input("enter a: "))
-    b = int(input("enter b: "))
-    n = int(input("enter n: "))
-    m = int(input("enter m: "))
-
-    array = np.zeros((n, m), dtype=np.float128)
-    array[0][0] = 0.5 * (b - a) * (f(a) + f(b))
+def rmbrg(
+    left_boundry: float,
+    right_boundry: float,
+    n: int,
+    m: int,
+    function: Callable[[float], float],
+):
+    array = np.zeros((n, m), dtype=np.longdouble)
+    array[0][0] = (
+        0.5
+        * (right_boundry - left_boundry)
+        * (function(left_boundry) + function(right_boundry))
+    )
 
     for i in range(n):
         if i != 0:
             sum = 0
             for k in range((2 ** (i - 1))):
-                sum += f(a + ((2 * (k + 1)) - 1) * ((b - a) / (2 ** i)))
+                sum += function(
+                    left_boundry
+                    + ((2 * (k + 1)) - 1)
+                    * ((right_boundry - left_boundry) / (2 ** i))
+                )
             array[i][0] = (0.5 * array[i - 1][0]) + (
-                (((b - a) / (2 ** i))) * sum
+                (((right_boundry - left_boundry) / (2 ** i))) * sum
             )
 
         for j in range(min(m, i + 1)):
@@ -28,12 +38,21 @@ def rmbrg():
                     array[i][j - 1] - array[i - 1][j - 1]
                 )
 
-    for n in array:
-        print(n)
+    return array
 
 
-def f(x) -> float:
+def g(x: float) -> float:
     return 4 / (1 + (x ** 2))
 
 
-rmbrg()
+def main():
+    a = int(input("enter a: "))
+    b = int(input("enter b: "))
+    n = int(input("enter n: "))
+    m = int(input("enter m: "))
+
+    print(rmbrg(a, b, n, m, g))
+
+
+if __name__ == "__main__":
+    main()
