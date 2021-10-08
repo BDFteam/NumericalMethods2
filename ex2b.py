fb86f685803221ea860b1879d748733783366584
Micha den Heijer <micha@michadenheijer.com>
2021-10-08 17:57:00 +0200

    Finish 2b

--- ex2b.py
+++ ex2b.py
@@ -1,48 +1,55 @@
 import numpy as np
 import numpy.random as rnd
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from scipy.optimize import ridder
+from scipy.stats import describe
 
-from ex2a import rmbg
 # print(rmbrg(a, b, n, m, lambda x: 4 / (1 + (x ** 2))))
 
 
 def main():
-    u = rnd.random()
+    # plot()
     n = 1000
     u_array = rnd.random(1000)
-    simulatesample(u)
-    # plot()
+    a_array = np.empty(n)
+
+    for i in range(n):
+        a_array[i] = simulatesample(u_array[i])
+
+    # plt.hist(a_array, 20, range=(0, 20))
+    # plt.show()
+
+    print(describe(a_array))
 
 
 def plot():
-    x = 0.1
-    i = 0
-    qq = np.empty(199)
-    qr = np.empty(199)
-    while x <= 20:
+    qr = np.linspace(0.1, 20, 1000)
+    qq = np.empty(1000)
+    for i, x in enumerate(qr):
         qq[i] = f(x)
         qr[i] = x
-        x += 0.1
-        i += 1
     plt.plot(qr, qq)
-    plt.xlim([0, 10])
-    plt.ylim([0, 3])
+    plt.xlim([0, 20])
     plt.grid(linestyle=":", linewidth=1.5)
     plt.show()
 
 
 def f(x):
     if x > 0:
-        part1 = (5 / (2 * np.pi * x ** 3)) ** 0.5
-        part2 = (-((x - 5) ** 2)) / (10 * x)
-        return part1 ** part2
+        part1 = np.sqrt(5 / (2 * np.pi * (x ** 3)))
+        part2 = -(((x - 5) ** 2) / (10 * x))
+        return part1 * np.exp(part2)
     return 0
 
 
-def simulatesample(u):
-    big_f = ?
-    big_f_solvable = big_f - u
-    
+def F(x):
+    return quad(f, -np.inf, x)[0]
+
+
+def simulatesample(u: float):
+    find_root_of = lambda x: F(x) - u
+    return ridder(find_root_of, 0, 100)
 
 
 if __name__ == "__main__":
